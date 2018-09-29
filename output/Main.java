5ff7bd06783e933df4eac7162bf6961020bce742
Your Name <bhanuka2017@gmail.com>
2018-09-29 19:02:55 +0530

    Fast Input Output Function Fix

--- output/Main.java
+++ output/Main.java
@@ -21,49 +21,88 @@ public class Main {
         OutputStream outputStream = System.out;
         input in = new input(inputStream);
         output out = new output(outputStream);
-        SandunsCourierService solver = new SandunsCourierService();
+        NeedHelp solver = new NeedHelp();
         solver.solve(1, in, out);
         out.close();
     }
 
-    static class SandunsCourierService {
+    static class NeedHelp {
         public void solve(int testNumber, input in, output out) {
-            int[] arr = {11, 2, 3, 4, 7, 9, 10, 22, 5, 1};
-            int min, max;
-            int n = arr.length;
-            int i = 0;
+            int t = ni();
+            int n = ni();
+            int m = ni();
+            int cl = ni();
+            int cr = ni();
 
-            if (n % 2 == 0) {
-                if (arr[0] > arr[1]) {
-                    max = arr[0];
-                    min = arr[1];
-                } else {
-                    min = arr[0];
-                    max = arr[1];
-                }
-                i = 2;
-            } else {
-                min = arr[0];
-                max = arr[0];
-                i = 1;
+            graphUnweightMatrix g = new graphUnweightMatrix(n);
+
+            for (int i = 0; i < m; i++) {
+                g.addUndirectedEdge(ni(), ni());
             }
 
-            for (; i < n - 1; i += 2) {
-                if (arr[i] > arr[i + 1]) {
-                    if (arr[i] > max)
-                        max = arr[i];
-                    if (arr[i + 1] < min)
-                        min = arr[i + 1];
-                } else {
-                    if (arr[i + 1] > max)
-                        max = arr[i + 1];
-                    if (arr[i] < min)
-                        min = arr[i];
+            g.DFSAll(0);
+        }
+
+        private int ni() {
+            return in.nextInt();
+        }
+
+    }
+
+    static class output {
+        private final PrintWriter writer;
+
+        public output(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public output(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+    }
+
+    static class graphUnweightMatrix {
+        private boolean[][] edges;
+        private int n;
+        private StringBuilder Traversal;
+
+        public graphUnweightMatrix(int nodes) {
+            n = nodes;
+            edges = new boolean[n][n];
+        }
+
+        public void addUndirectedEdge(int start, int end) {
+            edges[start][end] = true;
+            edges[end][start] = true;
+        }
+
+        public String DFSAll(int startIndex) {
+            Traversal = new StringBuilder();
+            boolean[] visited = new boolean[n];
+
+            for (int i = 0; i < n; i++)
+                if (!visited[i])
+                    DFSUtil(i, visited, Traversal);
+
+            Traversal.deleteCharAt(Traversal.length() - 1);
+            return Traversal.toString();
+        }
+
+        private void DFSUtil(int s, boolean[] visited, StringBuilder Traversal) {
+            visited[s] = true;
+            Traversal.append(s);
+            Traversal.append(" ");
+
+            for (int i = 0; i < n; i++) {
+                if (edges[s][i] && !visited[i]) {
+                    DFSUtil(i, visited, Traversal);
                 }
             }
-
-            out.print(min, max);
-
         }
 
     }
@@ -95,32 +134,6 @@ public class Main {
             }
             buffer = new byte[BUFFER_SIZE];
             bufferPointer = bytesRead = 0;
-        }
-
-    }
-
-    static class output {
-        private final PrintWriter writer;
-
-        public output(OutputStream outputStream) {
-            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
-        }
-
-        public output(Writer writer) {
-            this.writer = new PrintWriter(writer);
-        }
-
-        public void print(Object... objects) {
-            for (int i = 0; i < objects.length; i++) {
-                if (i != 0) {
-                    writer.print(' ');
-                }
-                writer.print(objects[i]);
-            }
-        }
-
-        public void close() {
-            writer.close();
         }
 
     }
